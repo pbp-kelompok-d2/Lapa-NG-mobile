b440cb2d091e57c63354f0109195fa8c8f1fee01
spudac <iamaspuduck@gmail.com>
2025-12-01 11:31:53 +0700

    adding authentication functionality

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:lapang/screens/home/home_page.dart';
+import 'package:lapang/screens/auth/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.green,
           ).copyWith(secondary: Colors.green[400]),
         ),
-        home: const HomePage(),
+        home: const LoginPage(),
       ),
     );
   }
